8490865de4f2c4da43477e5d71e32ca7a33439ba
Edwin Osvaldo <wini_omg_1402@hotmail.com>
2014-01-20 19:28:05 -0600

    Update LenguajesMoralesOsvaldo.java

--- lenguajesMoralesOsvaldo/src/lenguajesmoralesosvaldo/LenguajesMoralesOsvaldo.java
+++ lenguajesMoralesOsvaldo/src/lenguajesmoralesosvaldo/LenguajesMoralesOsvaldo.java
@@ -6,6 +6,7 @@ public class LenguajesMoralesOsvaldo {
     
     public static void main(String[] args) {
     System.out.println("Bienvenidos al Curso de Lenguajes!!");
+    System.out.println("Hola como estas");
     }
     
 }
