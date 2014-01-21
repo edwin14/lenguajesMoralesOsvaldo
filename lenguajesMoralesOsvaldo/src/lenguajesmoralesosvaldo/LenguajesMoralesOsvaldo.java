29f67497359670cc580600343f1724dfda7835e0
T107 <T107@PC257>
2014-01-20 19:14:01 -0600

    Primera Practica

---
+++ lenguajesMoralesOsvaldo/src/lenguajesmoralesosvaldo/LenguajesMoralesOsvaldo.java
@@ -0,0 +1,11 @@
+package lenguajesmoralesosvaldo;
+
+
+public class LenguajesMoralesOsvaldo {
+
+    
+    public static void main(String[] args) {
+    System.out.println("Bienvenidos al Curso de Lenguajes!!");
+    }
+    
+}
